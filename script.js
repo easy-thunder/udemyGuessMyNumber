7ae0acd662222006aa78605cc5fd678cab18f376
Jacob David Diehl <jakediehl17@gmail.com>
2022-11-29 14:02:39 -0700

    add game logic

--- script.js
+++ script.js
@@ -16,19 +16,50 @@ console.log( document.querySelector('.guess').value);
 
 */
 
-// events video 73
+// events video 73, game logic video74,
+const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
 
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener(`click`, function () {
     const guess = Number(document.querySelector('.guess').value);
 
     console.log(guess, typeof guess);
     if(!guess){
-        document.querySelector(`.message`).textContent = 'No Number!';
+        document.querySelector(`.message`).textContent = 'No Number!';}
+        else if (guess === secretNumber){
+            document.querySelector('.message').textContent = 'Correct Number';
+            
+        }
+        else if(guess > secretNumber){
+            if (score>1){
+            document.querySelector('.message').textContent = 'You guessed too high';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+                document.querySelector('.message').textContent = 'You lost the game';
+                document.querySelector('.score').textContent = 0;
+                
+            }
+        }
         
-    }
+        else if(guess < secretNumber){
+            if (score>1){
+                document.querySelector('.message').textContent = 'You guessed too high';
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+            else{
+                document.querySelector('.message').textContent = 'You lost the game';
+                document.querySelector('.score').textContent = 0;
+
+            }
+        }
+    
 });
 //===================================
-// Implement game Logic video 74
+
 
 
 
